773493a8cfeb7ab6ba980edb439d2b88cda36e34
Sam Anglin <s.anglin@live.co.uk>
2021-09-28 22:31:11 +0000

    primary css done

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,21 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-4">
-        <a href="/projects/create">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
 
-    <div class="flex flex-wrap -mx-3">
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded shadow" style="height: 200px">
-                    <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-
-                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
-                </div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                @include ('projects.card')
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
